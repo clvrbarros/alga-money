ee7e0a3c05e808e0b255f6d6c05a662f0fa72c3f
Cleverson Barros <cleversonferreirab@gmail.com>
2020-11-01 22:54:04 -0300

    created first request to category resource

---
+++ algamoney-api/src/main/java/com/example/algamoney/api/repository/CategoryRepository.java
@@ -0,0 +1,10 @@
+package com.example.algamoney.api.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.algamoney.api.model.Category;
+
+//Jpa needs to know the Entity and the type of the primary key 
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
