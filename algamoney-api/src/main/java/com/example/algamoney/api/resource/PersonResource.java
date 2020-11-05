aae3db0ce5cbac936cba310e91b38e681b9b419b
Cleverson Barros <cleversonferreirab@gmail.com>
2020-11-04 22:31:03 -0300

    add remove method

--- algamoney-api/src/main/java/com/example/algamoney/api/resource/PersonResource.java
+++ algamoney-api/src/main/java/com/example/algamoney/api/resource/PersonResource.java
@@ -10,13 +10,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.example.algamoney.api.event.ResourceCreatedEvent;
 import com.example.algamoney.api.model.Person;
@@ -57,4 +58,9 @@ public class PersonResource {
 		return ResponseEntity.status(HttpStatus.CREATED).body(personSaved);
 	}
 	
+	@DeleteMapping("/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void remove(@PathVariable Long id) {
+		personRepository.deleteById(id);
+	}
 }
